0b7a5641cfc96acb2710feb173716d0c25009f9c
CaiNot <303487454@qq.com>
2018-03-31 15:59:57 +0800

    Important Change Unfinished!

--- src/Search/TabuSearch.java
+++ src/Search/TabuSearch.java
@@ -9,14 +9,14 @@ import java.util.ArrayList;
  */
 public class TabuSearch {
 
-    private Vex vex = null;
+    private Vex vex = null, vex_outside_tabu = null, vex_in_tabu = null;
     private int k = 0;
-
-
     private Graph graph = null;
     private Vex[] vexes_tabu = null;
-    private int best_f = 0;
+    private int best_f = 0,color=0;
     private int f = 0, delt = 10000, delt_tabu = 10000;
+    private int color_after_in_tabu = 0, color_after_outside_tabu = 0;
+    private int best_count_in_tabu = 0, best_count_outside_tabu = 0;
 
     /**
      * @param graph_matrix 矩阵图
@@ -71,11 +71,10 @@ public class TabuSearch {
      * @return
      */
     public int findMove(int step) {
-        Vex vex = null, vex_outside_tabu = null,vex_in_tabu=null;
+        Vex vex = null;
 
         int color_before = 0;
-        int color_after_in_tabu = 0, color_after_outside_tabu = 0;
-        int best_count_in_tabu = 0, best_count_outside_tabu = 0;
+
         int neightbor_size = 0;
         int tmp = 0;
 
@@ -84,10 +83,7 @@ public class TabuSearch {
             if (vex.calcEnemyValue() == 0) {
                 continue;
             }
-
             color_before = vex.getColor();
-            neightbor_size = vex.calcNeighborValue();
-
             for (int i = 0; i < this.k; i++) {
                 if (i != color_before) {
                     tmp = vex.ifMove(color_before, i);
@@ -100,7 +96,7 @@ public class TabuSearch {
                             best_count_in_tabu++;
                             if (1 > (int) (Math.random() * best_count_in_tabu)) {
                                 vex_in_tabu = vex;
-                                color_after_outside_tabu = i;
+                                color_after_in_tabu = i;
                             }
                         }
                     } else {
@@ -118,21 +114,26 @@ public class TabuSearch {
                     }
                 }
             }
+        }
+        if(delt_tabu<f-best_f&&delt_tabu<delt){
+            delt=delt_tabu;
+            this.vex=this.vex_in_tabu;
+            this.color=this.color_after_in_tabu;
+        }else {
+            this.vex=this.vex_outside_tabu;
+            this.color=this.color_after_outside_tabu;
+        }
+        return 0;
+    }
 
+    public void makeMove(){
+        f=delt+f;
+        if(f<best_f){
+            best_f=f;
         }
 
     }
 
-    public int move_tabu(int step) {
-        this.vexes_tabu = new Vex[this.graph.vexes.length];
-        Vex v = null;
-        int best_value = 0, cur_value = 0;
-        for (int i = 0; i < this.graph.vexes.length; i++) {
-            v = this.graph.vexes[i];
-            if (v.isTabu(step))
-                vexes_tabu[i] = v;
-        }
-    }
 
     public Graph getGraph() {
         return graph;
